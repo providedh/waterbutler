237ef0418c7ae58563a4eac470eb158939f7e260
bug-rancher <bug-rancher@protonmail.com>
2019-03-21 10:11:31 +0100

    refactored corrector_date.py

--- waterbutler/providers/osfstorage/corrector_date.py
+++ waterbutler/providers/osfstorage/corrector_date.py
@@ -10,10 +10,8 @@ class CorrectorDate(AbstractCorrector):
     def _get_declarations_to_switch(self, attribute_name, attribute_value):
         attribute_value_without_space = attribute_value.replace(' ', '')
 
-        # TODO: refactor
         if "AM" in attribute_value or "B.C." in attribute_value or "BC" in attribute_value:
-            old_declaration = attribute_name + '=' + attribute_value
-            new_declaration = attribute_name + '-custom=' + attribute_value
+            old_declaration, new_declaration = self.get_default_declarations(attribute_name, attribute_value)
 
             return old_declaration, new_declaration
 
@@ -28,18 +26,14 @@ class CorrectorDate(AbstractCorrector):
 
             if date_1['y'] and date_1['m'] and date_1['d'] and date_2['y'] and date_2['m'] and date_2['d']:
                 old_declaration = attribute_name + '=' + attribute_value
-
                 new_declaration = 'from="{:04d}-{:02d}-{:02d}" to="{:04d}-{:02d}-{:02d}" {}-custom={}'.format(
                     date_1['y'], date_1['m'], date_1['d'], date_2['y'], date_2['m'], date_2['d'],
                     attribute_name, attribute_value)
 
-                return old_declaration, new_declaration
-
             else:
-                old_declaration = attribute_name + '=' + attribute_value
-                new_declaration = attribute_name + '-custom=' + attribute_value
+                old_declaration, new_declaration = self.get_default_declarations(attribute_name, attribute_value)
 
-                return old_declaration, new_declaration
+            return old_declaration, new_declaration
 
         # "aaa1234-1234-1234/1234-1234bbb"
         elif re.match(r'"\D*?([\d]{1,4})-([\d]{1,4})-([\d]{1,4})/([\d]{1,4})-([\d]{1,4})\D*?"',
@@ -52,17 +46,13 @@ class CorrectorDate(AbstractCorrector):
 
             if date_1['y'] and date_1['m'] and date_1['d'] and date_2['m'] and date_2['d']:
                 old_declaration = attribute_name + '=' + attribute_value
-
                 new_declaration = 'from="{0:04d}-{1:02d}-{2:02d}" to="{0:04d}-{3:02d}-{4:02d}" {5}-custom={6}'.format(
                     date_1['y'], date_1['m'], date_1['d'], date_2['m'], date_2['d'], attribute_name, attribute_value)
 
-                return old_declaration, new_declaration
-
             else:
-                old_declaration = attribute_name + '=' + attribute_value
-                new_declaration = attribute_name + '-custom=' + attribute_value
+                old_declaration, new_declaration = self.get_default_declarations(attribute_name, attribute_value)
 
-                return old_declaration, new_declaration
+            return old_declaration, new_declaration
 
         # "aaa1234-1234-1234/1234bbb"
         elif re.match(r'"\D*?([\d]{1,4})-([\d]{1,4})-([\d]{1,4})/([\d]{1,4})\D*?"',
@@ -75,17 +65,13 @@ class CorrectorDate(AbstractCorrector):
 
             if date_1['y'] and date_1['m'] and date_1['d'] and date_2['d']:
                 old_declaration = attribute_name + '=' + attribute_value
-
                 new_declaration = 'from="{0:04d}-{1:02d}-{2:02d}" to="{0:04d}-{1:02d}-{3:02d}" {4}-custom={5}'.format(
                     date_1['y'], date_1['m'], date_1['d'], date_2['d'], attribute_name, attribute_value)
 
-                return old_declaration, new_declaration
-
             else:
-                old_declaration = attribute_name + '=' + attribute_value
-                new_declaration = attribute_name + '-custom=' + attribute_value
+                old_declaration, new_declaration = self.get_default_declarations(attribute_name, attribute_value)
 
-                return old_declaration, new_declaration
+            return old_declaration, new_declaration
 
         # "aaa1234-1234-1234bbb"
         elif re.match(r'"\D*?([\d]{1,4})-([\d]{1,4})-([\d]{1,4})\D*?"', attribute_value_without_space):
@@ -94,13 +80,14 @@ class CorrectorDate(AbstractCorrector):
             date = self.recognize_date_parts(match.group(1), match.group(2), match.group(3))
 
             if date['y'] and date['m'] and date['d']:
-                correct_date = "{:04d}-{:02d}-{:02d}".format(date['y'], date['m'], date['d'])
+                old_declaration = attribute_name + '=' + attribute_value
+                new_declaration = '{3}="{0:04d}-{1:02d}-{2:02d}" {3}-custom={4}'.format(
+                    date['y'], date['m'], date['d'], attribute_name, attribute_value)
 
             else:
-                old_declaration = attribute_name + '=' + attribute_value
-                new_declaration = attribute_name + '-custom=' + attribute_value
+                old_declaration, new_declaration = self.get_default_declarations(attribute_name, attribute_value)
 
-                return old_declaration, new_declaration
+            return old_declaration, new_declaration
 
         # "aaa1234-1234bbbb"
         elif re.match(r'"\D*?([\d]{1,4})-([\d]{1,4})\D*?"', attribute_value_without_space):
@@ -109,12 +96,14 @@ class CorrectorDate(AbstractCorrector):
             date = self.recognize_date_parts(match.group(1), match.group(2))
 
             if date['y'] and date['m']:
-                correct_date = "{:04d}-{:02d}".format(date['y'], date['m'])
-            else:
                 old_declaration = attribute_name + '=' + attribute_value
-                new_declaration = attribute_name + '-custom=' + attribute_value
+                new_declaration = '{2}="{0:04d}-{1:02d}" {2}-custom={3}'.format(
+                    date['y'], date['m'], attribute_name, attribute_value)
 
-                return old_declaration, new_declaration
+            else:
+                old_declaration, new_declaration = self.get_default_declarations(attribute_name, attribute_value)
+
+            return old_declaration, new_declaration
 
         # "aaa1234bbb"
         elif re.match(r'"\D*?([\d]{1,4})\D*?"', attribute_value_without_space):
@@ -123,25 +112,19 @@ class CorrectorDate(AbstractCorrector):
             date = self.recognize_date_parts(match.group(1))
 
             if date['y']:
-                correct_date = "{:04d}".format(date['y'])
+                old_declaration = attribute_name + '=' + attribute_value
+                new_declaration = '{1}="{0:04d}" {1}-custom={2}'.format(
+                    date['y'], attribute_name, attribute_value)
 
             else:
-                old_declaration = attribute_name + '=' + attribute_value
-                new_declaration = attribute_name + '-custom=' + attribute_value
-
-                return old_declaration, new_declaration
-
-        else:
-            old_declaration = attribute_name + '=' + attribute_value
-            new_declaration = attribute_name + '-custom=' + attribute_value
+                old_declaration, new_declaration = self.get_default_declarations(attribute_name, attribute_value)
 
             return old_declaration, new_declaration
 
-        old_declaration = attribute_name + '=' + attribute_value
-        new_declaration = (attribute_name + '="' + correct_date + '"' + ' ' + attribute_name + '-custom=' +
-                           attribute_value)
+        else:
+            old_declaration, new_declaration = self.get_default_declarations(attribute_name, attribute_value)
 
-        return old_declaration, new_declaration
+            return old_declaration, new_declaration
 
     def recognize_date_parts(self, *args):
         year_candidates = []
@@ -174,3 +157,9 @@ class CorrectorDate(AbstractCorrector):
             date['d'] = day_candidates[0]
 
         return date
+
+    def get_default_declarations(self, attribute_name, attribute_value):
+        old_declaration = attribute_name + '=' + attribute_value
+        new_declaration = attribute_name + '-custom=' + attribute_value
+
+        return old_declaration, new_declaration
